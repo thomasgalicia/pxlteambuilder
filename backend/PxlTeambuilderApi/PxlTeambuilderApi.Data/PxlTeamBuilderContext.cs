941840a222c19f997164135c537f071303ad122f
Thomas Galicia <11601037@student.pxl.be>
2019-01-04 12:36:47 +0100

    initialize database context

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/PxlTeamBuilderContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PxlTeambuilderApi.Data.Domain;
+
+namespace PxlTeambuilderApi.Data
+{
+    public class PxlTeamBuilderContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<UserProjects> UserProjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //user table
+            modelBuilder.Entity<User>().HasKey(user => user.Email);
+
+            //project table
+            modelBuilder.Entity<Project>().HasKey(project => project.ProjectId);
+
+            //many to many relationship
+            modelBuilder.Entity<UserProjects>().HasKey(userProject => new { userProject.Email, userProject.ProjectId });
+            modelBuilder.Entity<UserProjects>().HasOne(userProject => userProject.User).WithMany(user => user.UserProjects).HasForeignKey(userProject => userProject.Email);
+            modelBuilder.Entity<UserProjects>().HasOne(userProject => userProject.Project).WithMany(project => project.UserProjects).HasForeignKey(userProject => userProject.ProjectId);
+        }
+
+    }
+}
