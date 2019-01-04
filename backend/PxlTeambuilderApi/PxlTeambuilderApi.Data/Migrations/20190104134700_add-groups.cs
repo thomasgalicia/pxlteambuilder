60b352c467970783604b866c3d0763eeaf5990fd
Unknown <11601037@student.pxl.be>
2019-01-04 14:49:49 +0100

    Add migration for "rework domain classes with groups"

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Migrations/20190104134700_add-groups.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PxlTeambuilderApi.Data.Migrations
+{
+    public partial class addgroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Group_Projects_ProjectId",
+                table: "Group");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_Group_GroupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Group",
+                table: "Group");
+
+            migrationBuilder.RenameTable(
+                name: "Group",
+                newName: "Groups");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Group_ProjectId",
+                table: "Groups",
+                newName: "IX_Groups_ProjectId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Groups",
+                table: "Groups",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Projects_ProjectId",
+                table: "Groups",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "ProjectId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_Groups_GroupId",
+                table: "UserGroups",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Projects_ProjectId",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_Groups_GroupId",
+                table: "UserGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Groups",
+                table: "Groups");
+
+            migrationBuilder.RenameTable(
+                name: "Groups",
+                newName: "Group");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Groups_ProjectId",
+                table: "Group",
+                newName: "IX_Group_ProjectId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Group",
+                table: "Group",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Group_Projects_ProjectId",
+                table: "Group",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "ProjectId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_Group_GroupId",
+                table: "UserGroups",
+                column: "GroupId",
+                principalTable: "Group",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
