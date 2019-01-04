a64d25fa2ae51c86840e2719843f917e4a3e7a4d
Thomas Galicia <11601037@student.pxl.be>
2019-01-04 12:10:55 +0100

    add domain classes and userroles

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using PxlTeambuilderApi.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PxlTeambuilderApi.Data.Domain
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; } 
+
+    }
+}
---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Enums/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PxlTeambuilderApi.Data.Enums
+{
+    public enum UserRole
+    {
+        Student,Teacher
+    }
+}
