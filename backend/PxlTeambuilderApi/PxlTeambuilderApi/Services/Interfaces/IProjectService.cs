57ee99da108b904924522f07d16067334d240720
Thomas Galicia <11601037@student.pxl.be>
2019-01-04 13:39:47 +0100

    add IUserService and IProjectService

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,17 @@
+﻿using PxlTeambuilderApi.Data.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Services.Interfaces
+{
+    public interface IProjectService
+    {
+
+        Project GetProjectById(int projectId);
+        Project AddProject(Project project);
+        Project UpdateProject(int projectId, Project project);
+
+    }
+}
