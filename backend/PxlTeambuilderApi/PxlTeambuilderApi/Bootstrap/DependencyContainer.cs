e24d698089747dac4b385ed2f5970d8ce0813f2a
Unknown <11601037@student.pxl.be>
2019-01-04 20:43:54 +0100

    Add autofac dependency injection Api

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Bootstrap/DependencyContainer.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using PxlTeambuilderApi.Data;
+using PxlTeambuilderApi.Repositories.Implementations;
+using PxlTeambuilderApi.Repositories.Interfaces;
+using PxlTeambuilderApi.Services.Implementations;
+using PxlTeambuilderApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Bootstrap
+{
+    public class DependencyContainer
+    {
+
+        public static ContainerBuilder RegisterDependencies(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+
+            //repositories
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<ProjectRepository>().As<IProjectRepository>().InstancePerLifetimeScope();
+
+            //services
+            builder.RegisterType<UserService>().As<IUserService>().InstancePerLifetimeScope();
+            builder.RegisterType<ProjectService>().As<IProjectService>().InstancePerLifetimeScope();
+
+            //othes
+            builder.RegisterType<PxlTeamBuilderContext>().InstancePerLifetimeScope();
+
+            builder.Populate(services);
+            return builder;
+        }
+
+    }
+}
--- backend/PxlTeambuilderApi/PxlTeambuilderApi/Startup.cs
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Startup.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PxlTeambuilderApi.Bootstrap;
+using PxlTeambuilderApi.Data;
+using PxlTeambuilderApi.Data.Domain;
 
 namespace PxlTeambuilderApi
 {
     public class Startup
     {
+        public IContainer ApplicationContainer { get; private set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -21,9 +28,24 @@ namespace PxlTeambuilderApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+           /* PxlTeamBuilderContext context = new PxlTeamBuilderContext();
+            User user = new User
+            {
+                Email = "thomas@student.pxl.be",
+                Name = "thomas",
+                Password = "thomas",
+                Role = Data.Enums.UserRole.Student
+            };
+            context.Users.Add(user);
+            context.SaveChanges();*/
+
+            var builder = DependencyContainer.RegisterDependencies(services);
+            this.ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(this.ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +57,7 @@ namespace PxlTeambuilderApi
             }
 
             app.UseMvc();
+
         }
     }
 }
