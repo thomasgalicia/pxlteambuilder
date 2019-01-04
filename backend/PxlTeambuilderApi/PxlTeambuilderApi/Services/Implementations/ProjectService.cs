a71464b6d22ee2fb2207a790561da87a20819a73
Unknown <11601037@student.pxl.be>
2019-01-04 14:05:40 +0100

    Add async keyword to methods of ProjectService

--- backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Implementations/ProjectService.cs
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Implementations/ProjectService.cs
@@ -9,20 +9,20 @@ namespace PxlTeambuilderApi.Services.Implementations
 {
     public class ProjectService : IProjectService
     {
-        public Task<Project> GetProjectByIdAsync(int projectId)
+        public async Task<Project> GetProjectByIdAsync(int projectId)
         {
             throw new NotImplementedException();
         }
 
         //TODO: implement
-        public Task<Project> AddProjectAsync(Project project)
+        public async Task<Project> AddProjectAsync(Project project)
         {
             throw new NotImplementedException();
         }
 
         //TODO: implement
         //TODO: check max amount of students per group for project
-        public Task<Project> UpdateProjectAsync(int projectId, Project project)
+        public async Task<Project> UpdateProjectAsync(int projectId, Project project)
         {
             throw new NotImplementedException();
         }
