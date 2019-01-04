57dbbf8dc0a1639b1272a2ed61cf57f858d58cd4
Unknown <11601037@student.pxl.be>
2019-01-04 15:20:35 +0100

    Add User- and ProjectController

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PxlTeambuilderApi.Services.Interfaces;
+
+namespace PxlTeambuilderApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/projects")]
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            this.projectService = projectService;
+        }
+
+      
+      
+    }
+}
---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PxlTeambuilderApi.Data.Domain;
+using PxlTeambuilderApi.Services.Interfaces;
+
+namespace PxlTeambuilderApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        [Route("{userId}/projects")]
+        public async Task<IActionResult> GetAllProjectsFromUser(int userId)
+        {
+            User user = await userService.GetUserByUserIdAsync(userId);
+            return Ok(user.Projects);
+        }
+
+    }
+}
