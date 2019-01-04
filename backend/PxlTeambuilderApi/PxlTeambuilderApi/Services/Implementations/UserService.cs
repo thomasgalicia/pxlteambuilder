7bbd7c1f9b9f2303300a6d6a9c3199a5c573f410
Unknown <11601037@student.pxl.be>
2019-01-04 14:04:12 +0100

    Made IUserservice and UserService asynchronous

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Implementations/UserService.cs
@@ -0,0 +1,32 @@
+﻿using PxlTeambuilderApi.Data.Domain;
+using PxlTeambuilderApi.Repositories.Interfaces;
+using PxlTeambuilderApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        //TODO: throw exception when user not found (=null)
+        public Task<User> GetUserByEmailAsync(string email)
+        {
+            return userRepository.GetUserByEmail(email);
+        }
+
+        //TODO: hash password before adding to database
+        public Task<User> AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Interfaces/IUserService.cs
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Interfaces/IUserService.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using PxlTeambuilderApi.Data.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PxlTeambuilderApi.Services.Interfaces
 {
-    interface IUserService
+    public interface IUserService
     {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<User> AddUserAsync(User user);
     }
 }
