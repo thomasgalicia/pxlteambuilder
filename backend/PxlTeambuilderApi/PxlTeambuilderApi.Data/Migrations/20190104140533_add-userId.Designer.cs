d614c54d0c1bcbe847d859d042ef8ec393031941
Unknown <11601037@student.pxl.be>
2019-01-04 15:07:26 +0100

    Add migration for "add userId to User class"

--- backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Migrations/20190104140533_add-userId.Designer.cs
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Migrations/20190104140533_add-userId.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PxlTeambuilderApi.Data;
 
 namespace PxlTeambuilderApi.Data.Migrations
 {
     [DbContext(typeof(PxlTeamBuilderContext))]
-    partial class PxlTeamBuilderContextModelSnapshot : ModelSnapshot
+    [Migration("20190104140533_add-userId")]
+    partial class adduserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Migrations/20190104140533_add-userId.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PxlTeambuilderApi.Data.Migrations
+{
+    public partial class adduserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_UserEmail",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_Users_Email",
+                table: "UserGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_UserEmail",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "UserGroups",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups",
+                columns: new[] { "UserId", "GroupId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_UserId",
+                table: "Projects",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_Users_UserId",
+                table: "UserGroups",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_UserId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserGroups_Users_UserId",
+                table: "UserGroups");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_UserId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "UserGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "UserGroups",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "Projects",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Email");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserGroups",
+                table: "UserGroups",
+                columns: new[] { "Email", "GroupId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_UserEmail",
+                table: "Projects",
+                column: "UserEmail");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_UserEmail",
+                table: "Projects",
+                column: "UserEmail",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserGroups_Users_Email",
+                table: "UserGroups",
+                column: "Email",
+                principalTable: "Users",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
