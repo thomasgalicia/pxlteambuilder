67aa05361a4b564197e128e4e42699bcd8dd8b39
Unknown <11601037@student.pxl.be>
2019-01-04 18:04:50 +0100

    Add ProjectNotFoundException

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Exceptions/ProjectNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Exceptions
+{
+    public class ProjectNotFoundException : Exception
+    {
+
+        public ProjectNotFoundException(string projectId) : base($"project with id:{projectId} was not found")
+        {
+
+        }
+    }
+}
