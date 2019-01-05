5d60f4edbf23fd17f52f577291c10781a358cc86
thomasgalicia <11601037@student.pxl.be>
2019-01-05 14:32:41 +0100

    add exceptions

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Exceptions/MaxStudentsBoundsException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Exceptions
+{
+    public class MaxStudentsBoundsException : Exception
+    {
+
+        public MaxStudentsBoundsException() : base()
+        {
+
+        }
+
+    }
+}
