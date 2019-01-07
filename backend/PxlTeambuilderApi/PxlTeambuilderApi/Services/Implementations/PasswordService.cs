7d2e5c507983e2a5bd5eec0128c3bfe994cf39bd
Thomas Galicia <11601037@student.pxl.be>
2019-01-07 11:16:28 +0100

    add passwordservice

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Implementations/PasswordService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using PxlTeambuilderApi.Services.Interfaces;
+
+namespace PxlTeambuilderApi.Services.Implementations
+{
+    public class PasswordService : IPasswordService
+    {
+        public string GenerateHash(string rawPassword)
+        {
+            
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+               byte[] textData = System.Text.Encoding.UTF8.GetBytes(rawPassword);
+               byte[] hash = sha.ComputeHash(textData);
+               return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+            
+        }
+
+        public bool IsSame(string rawPassword, string hashPassword)
+        {
+            string generatedHash = GenerateHash(rawPassword);
+            return rawPassword == hashPassword;
+        }
+    }
+}
---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Services/Interfaces/IPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Services.Interfaces
+{
+    public interface IPasswordService
+    {
+
+        string GenerateHash(string rawPassword);
+
+        bool IsSame(string rawPassword, string hashPassword);
+    }
+}
