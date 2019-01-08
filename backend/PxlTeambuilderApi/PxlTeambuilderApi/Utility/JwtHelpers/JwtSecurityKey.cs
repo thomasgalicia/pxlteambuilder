10def64b10f64beccad54f06ea3fd48ae99be01b
Thomas Galicia <11601037@student.pxl.be>
2019-01-08 19:09:58 +0100

    add jwt helpers

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Utility/JwtHelpers/JwtSecurityKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PxlTeambuilderApi.Utility.JwtHelpers
+{
+    public static class JwtSecurityKey
+    {
+
+        public static SymmetricSecurityKey Create(string secret)
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+        }
+
+    }
+}
---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi/Utility/JwtHelpers/JwtToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PxlTeambuilderApi.Utility.JwtHelpers
+{
+    public class JwtToken
+    {
+
+        private JwtSecurityToken token;
+
+        internal JwtToken(JwtSecurityToken token)
+        {
+            this.token = token;
+        }
+
+        public DateTime ValidTo => token.ValidTo;
+        public string Value => new JwtSecurityTokenHandler().WriteToken(this.token);
+
+    }
+}
