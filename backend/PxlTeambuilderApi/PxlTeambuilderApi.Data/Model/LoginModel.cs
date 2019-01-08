6acb148c6991308abc2271f5ce817a70d1d65080
Thomas Galicia <11601037@student.pxl.be>
2019-01-08 19:11:29 +0100

    add loginmodel

---
+++ backend/PxlTeambuilderApi/PxlTeambuilderApi.Data/Model/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PxlTeambuilderApi.Data.Model
+{
+    public class LoginModel
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+    }
+}
