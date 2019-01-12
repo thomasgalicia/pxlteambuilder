dd6e6202773e31ea7593234118e2bf2a53b2225d
thomasgalicia <11601037@student.pxl.be>
2019-01-12 14:42:50 +0100

    add create project functionality to create project component

--- frontend/pxlTeambuilder/src/app/createproject/createproject.component.ts
+++ frontend/pxlTeambuilder/src/app/createproject/createproject.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProjectService } from '../services/project/project.service';
+import { projection } from '@angular/core/src/render3';
+import { Project } from '../models/project';
+import { AuthService } from '../services/auth/auth.service';
+import { HttpResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-createproject',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateprojectComponent implements OnInit {
 
-  constructor() { }
+  private form : FormGroup
+  private loading : boolean = false;
 
-  ngOnInit() {
+  constructor(private formbuilder: FormBuilder, private project : ProjectService, private auth : AuthService, private toast : ToastrService) {
+    
   }
 
+  ngOnInit() {
+    this.form = this.formbuilder.group({
+      "title" : ['',Validators.required],
+      "description": [''],
+      'maxstudentspergroup' : [4,Validators.min(1)]
+    });
+    
+  }
+
+  public createProject(){
+    this.loading = true;
+    let project = new Project();
+    let {title, description, maxstudentspergroup} = this.form.controls;
+    project.Title = title.value;
+    project.Description = description.value;
+    project.MaxStudentsPerGroup = maxstudentspergroup.value;
+    project.UserId = this.auth.User.Id;
+    
+    this.project.addProject(project).subscribe(data => this.handleSuccess(), error => this.handleError() );
+  }
+
+  private handleSuccess(){
+    this.loading = false;
+    this.toast.success("Project was succesfully saved","Success")
+    this.resetForm()
+  }
+
+  private handleError(){
+    this.loading = false;
+    this.toast.error("Something went wrong when adding project","Oops")
+  }
+
+  private resetForm(){
+    let {title, description, maxstudentspergroup} = this.form.controls;
+    title.setValue('');
+    description.setValue("");
+    maxstudentspergroup.setValue(4);
+  }
 }
